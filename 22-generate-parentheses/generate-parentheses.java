b9d8518b218d6ab6995719715d2732f168c3f22a
Shubham Gupta <shubhcs01@gmail.com>
2025-07-30 00:40:03 +0530

    Time: 50 ms (5.25%) | Memory: 45.3 MB (5.07%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,21 +1,35 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> ans = new ArrayList<>();
-        backtrack("", 0, 0, n, ans);
+        StringBuilder sb = new StringBuilder();
+        backtrack(sb, n * 2, ans);
         return ans;
     }
 
-    private void backtrack(String str, int open, int close, int n, List<String> ans) {
-        if (str.length() == n * 2) {
-            ans.add(str);
+    private void backtrack(StringBuilder sb, int n, List<String> ans) {
+        //base case
+        if (sb.length() == n) {
+            if (isValid(sb.toString())) {
+                ans.add(sb.toString());
+            }
             return;
         }
 
-        if (open < n) {
-            backtrack(str + "(", open + 1, close, n, ans);
+        backtrack(sb.append('('), n, ans);
+        sb.deleteCharAt(sb.length() - 1);
+        backtrack(sb.append(')'), n, ans);
+        sb.deleteCharAt(sb.length() - 1);
+    }
+
+    private boolean isValid(String str) {
+        Stack<Character> st = new Stack<>();
+        for (char ch : str.toCharArray()) {
+            if (st.size() > 0 && ch == ')' && st.peek() == '(') {
+                st.pop();
+                continue;
+            }
+            st.push(ch);
         }
-        if (close < open) {
-            backtrack(str + ")", open, close + 1, n, ans);
-        }
+        return st.isEmpty();
     }
 }
