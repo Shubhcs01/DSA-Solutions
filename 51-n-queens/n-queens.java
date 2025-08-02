4273ba3f7a4e7d73170a91fcf6157e6f426c7413
Shubham Gupta <shubhcs01@gmail.com>
2025-08-02 20:14:57 +0530

    Time: 2 ms (86.72%) | Memory: 44.8 MB (88.55%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,57 +1,75 @@
 class Solution {
-    public List<List<String>> ans = new ArrayList<>();
-
     public List<List<String>> solveNQueens(int n) {
-        boolean[][] board = new boolean[n][n];  
-        backtrack(board, 0);                      
+        char[][] board = new char[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = '.';
+            }
+        }
+
+        List<List<String>> ans = new ArrayList<>();
+        backtrack(board, 0, ans);
         return ans;
     }
 
-    public void backtrack(boolean[][] board, int row) {
-        //base case
-        if (row == board.length) {
-            display(board);                     
+    private void backtrack(char[][] board, int col, List<List<String>> ans) {
+        // base case
+        if (col == board[0].length) {
+            ans.add(construct(board));
             return;
         }
 
-        for (int col = 0; col < board.length; col++) {
+        for (int row = 0; row < board.length; row++) {
             if (isSafe(board, row, col)) {
-                board[row][col] = true;         
-                backtrack(board, row + 1);         
-                board[row][col] = false;        
+                board[row][col] = 'Q';
+                backtrack(board, col + 1, ans);
+                board[row][col] = '.';
             }
         }
     }
 
-    public boolean isSafe(boolean[][] board, int row, int col) {
-      
-        for (int i = 0; i < row; i++) {
-            if (board[i][col]) return false;
+    private boolean isSafe(char[][] board, int row, int col) {
+        int duprow = row;
+        int dupcol = col;
+
+        // upper-left diagonal
+        while (row >= 0 && col >= 0) {
+            if (board[row][col] == 'Q')
+                return false;
+            row--;
+            col--;
         }
 
-        int minLeft = Math.min(row, col);
-        for (int i = 1; i <= minLeft; i++) {
-            if (board[row - i][col - i]) return false;
+        row = duprow;
+        col = dupcol;
+
+        // left row
+        while (col >= 0) {
+            if (board[row][col] == 'Q')
+                return false;
+            col--;
         }
 
-      
-        int minRight = Math.min(row, board.length - col - 1);
-        for (int i = 1; i <= minRight; i++) {
-            if (board[row - i][col + i]) return false;
-        }
+        row = duprow;
+        col = dupcol;
 
-        return true; 
+        // lower-left diagonal
+        while (col >= 0 && row < board.length) {
+            if (board[row][col] == 'Q')
+                return false;
+            col--;
+            row++;
+        }
+        return true;
     }
 
-    public void display(boolean[][] board) {
-        List<String> queenAns = new ArrayList<>();
-        for (boolean[] row : board) {
-            StringBuilder temp = new StringBuilder();
-            for (boolean cell : row) {
-                temp.append(cell ? 'Q' : '.');
-            }
-            queenAns.add(temp.toString());
+    private List<String> construct(char[][] board) {
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < board.length; i++) {
+            String s = new String(board[i]);
+            list.add(s);
         }
-        ans.add(queenAns);  
+        return list;
     }
 }
