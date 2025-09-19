e90f47a77638f20d0d74d2e0e451f3a314b7d85c
Shubham Gupta <shubhcs01@gmail.com>
2025-09-19 23:57:19 +0530

    Time: 112 ms (83.10%) | Memory: 56.6 MB (15.49%) - LeetSync

---
+++ 3797-design-spreadsheet/design-spreadsheet.java
@@ -0,0 +1,28 @@
+class Spreadsheet {
+
+    HashMap<String, Integer> map;
+
+    public Spreadsheet(int rows) {
+        map = new HashMap<>();
+    }
+
+    public void setCell(String cell, int value) {
+        map.put(cell, value);
+    }
+
+    public void resetCell(String cell) {
+        map.put(cell, 0);
+    }
+
+    public int getValue(String formula) {
+        String arr[] = formula.substring(1).split("\\+");
+        return getCell(arr[0]) + getCell(arr[1]);
+    }
+
+    public int getCell(String cell) {
+        if (cell.charAt(0) >= '0' && cell.charAt(0) <= '9') {
+            return Integer.parseInt(cell);
+        } else
+            return map.getOrDefault(cell, 0);
+    }
+}
