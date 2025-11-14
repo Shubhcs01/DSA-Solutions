163476c3290540e6b0f4fc365fbc5ac5120bf564
Shubham Gupta <shubhcs01@gmail.com>
2025-11-14 22:21:10 +0530

    Time: 6 ms (92.93%) | Memory: 46.4 MB (8.86%) - LeetSync

---
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int n = lcs(word1,word2);
+        return (word1.length() - n) + (word2.length() - n);
+    }
+
+    public int lcs(String text1, String text2) {
+        int[][] dp = new int[text1.length()][text2.length()];
+        for(int[] row: dp) Arrays.fill(row,-1);
+        return solve(text1.length()-1,text2.length()-1,text1,text2,dp);
+    }
+
+    private int solve(int i, int j, String str1, String str2, int[][] dp){
+        //base
+        if(i < 0 || j < 0) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+
+        if(str1.charAt(i) == str2.charAt(j)){
+            return dp[i][j] = 1 + solve(i - 1, j - 1,str1,str2,dp);
+        }
+
+        return dp[i][j] = 0 + Math.max(solve(i - 1, j, str1, str2,dp), solve(i, j - 1, str1, str2,dp));
+    }
+}
